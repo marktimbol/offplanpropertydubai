e0101a03f4bd804f6a684e0d90f0cb98f46ad7ec
Mark Timbol <Mark@Mark.local>
2016-10-18 18:08:45 +0400

    Bruh

--- resources/views/public/developers/show.blade.php
+++ resources/views/public/developers/show.blade.php
@@ -52,10 +52,19 @@
 			</div>
 			<div class="row">
 				@foreach( $developer->projects as $project )
+					<?php
+						$projectPhoto = $project->photos->first();
+
+						$photo = sprintf('https://s3-%s.amazonaws.com/%s/%s', 
+								config('filesystems.disks.s3.region'), 
+								config('filesystems.disks.s3.bucket'), 
+								$projectPhoto->photo
+						);
+					?>
 					<div class="col-md-4">
 						<div class="Project">
 							<div class="Project__image">
-								<img src="/images/projects/project.jpg" alt="{{ $project->name }}" title="{{ $project->name }}" class="img-responsive" />
+								<img src="{{ $photo }}" alt="{{ $project->name }}" title="{{ $project->name }}" class="img-responsive" />
 							</div>
 							<div class="Project__content Flex Flex--space-between">
 								<h4 class="Project__title">
