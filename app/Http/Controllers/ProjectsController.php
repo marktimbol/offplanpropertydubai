db2b29161257b5f4a415d99cc8460443e10c2c5d
Mark Timbol <Mark@Mark.local>
2016-10-23 14:13:10 +0400

    My goodness

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -15,7 +15,7 @@ class ProjectsController extends Controller
 
     	$logo = '/images/logo.png';
     	if( count($project->logos) > 0 ) {
-    		$logo = getPhotoPath($project->logos->phot);
+    		$logo = getPhotoPath($project->logos->photo);
     	}
     	return view('public.projects.show', compact('project', 'logo'));
     }
