1782f5322e4a5146c9c1e986392f883659af3cbc
Mark Timbol <Mark@Mark.local>
2016-10-23 14:14:49 +0400

    Are you serious?

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -14,8 +14,8 @@ class ProjectsController extends Controller
     	$project->load('developer');
 
     	$logo = '/images/logo.png';
-    	if( count($project->logos) > 0 ) {
-    		$logo = getPhotoPath($project->logos->photo);
+    	if( count($project->logo) > 0 ) {
+    		$logo = getPhotoPath($project->logo->photo);
     	}
     	return view('public.projects.show', compact('project', 'logo'));
     }
