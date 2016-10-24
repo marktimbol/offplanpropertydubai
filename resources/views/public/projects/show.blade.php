5e2ad5fb1e238a53110122e8fac321f6b11535c1
Mark Timbol <Mark@Mark.local>
2016-10-24 12:44:55 +0400

    Update

--- resources/views/public/projects/show.blade.php
+++ resources/views/public/projects/show.blade.php
@@ -94,64 +94,6 @@
 							Download Brochure
 						</button>
 					</p>
-					<div>
-						<h3>Developer</h3>
-					</div>
-					<div class="Inquiry">
-						<h3 class="Inquiry__title">Register your Interest</h3>
-						<form method="POST" action="{{ route('projects.inquiries.store', $project->slug) }}">
-							{{ csrf_field() }}
-							<div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
-								<label for="name">Name *</label>
-								<input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required/>
-								@if( $errors->has('name') )
-									<span class="help-block">
-										<strong>{{ $errors->first('name') }}</strong>
-									</span>
-								@endif
-							</div>
-							<div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
-								<label for="email">eMail *</label>
-								<input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}" required />
-								@if( $errors->has('email') )
-									<span class="help-block">
-										<strong>{{ $errors->first('email') }}</strong>
-									</span>
-								@endif
-							</div>
-							<div class="form-group {{ $errors->has('phone') ? 'has-error' : '' }}">
-								<label for="phone">Phone *</label>
-								<input type="text" name="phone" id="phone" class="form-control" value="{{ old('phone') }}" required />
-								@if( $errors->has('phone') )
-									<span class="help-block">
-										<strong>{{ $errors->first('phone') }}</strong>
-									</span>
-								@endif
-							</div>
-							<div class="form-group">
-								<label for="iam">I Am:</label>
-								<select name="iam" class="form-control">
-									<option value=""></option>
-									<option value="Investor">Investor</option>
-									<option value="Agent">Agent</option>
-									<option value="Guest">Guest</option>
-								</select>
-							</div>
-							<div class="form-group">
-								<label for="country">Country</label>
-								<input type="text" name="country" id="country" class="form-control" value="{{ old('country') }}" required />
-							</div>
-							<div class="form-group">
-								<label for="message">Message</label>
-								<textarea name="message" id="message" class="form-control">
-									{{ old('message') }}
-								</textarea>
-							</div>
-							<div class="form-group">
-								<button type="submit" class="btn btn-block btn-primary"><i class="fa fa-send"></i> &nbsp; Send Message</button>
-							</div>
-						</form>
-					</div>		
 				</div>
 			</div>
 		</div>
