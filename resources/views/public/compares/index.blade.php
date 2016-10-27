102d48a509ee8f66124d3a1cd647bfb62b3ce52d
Mark Timbol <mark.timbol@hotmail.com>
2016-10-27 11:01:52 +0400

    Select project to compare

--- resources/views/public/compares/index.blade.php
+++ resources/views/public/compares/index.blade.php
@@ -25,53 +25,59 @@
 						<th>&nbsp;</th>
 					</thead>
 					<tbody>
-						@foreach( $compares as $compare )
+						@if( count($compares) > 0 )
+							@foreach( $compares as $compare )
+								<tr>
+									<td width="300">
+										<a href="{{ route('projects.show', $compare->options->project->slug) }}">
+											{{ sprintf('%s - %s', $compare->options->project->name, $compare->options->project->title) }}
+										</a> 
+										<small>
+											@foreach( $compare->options->project->types as $type )
+												<span class="label label-success">{{ $type->name }}</span>
+											@endforeach
+										</small>
+									</td>
+									<td>
+										<a href="{{ route('developers.show', $compare->options->project->developer->slug) }}">
+											{{ $compare->options->project->developer->name }}
+										</a>
+									</td>
+									<td>
+										@foreach( $compare->options->project->communities as $community )
+											{{ $community->name }}
+										@endforeach
+									</td>
+									<td>
+										<table class="table table-bordered table-striped">
+											@foreach( $compare->options->project->payments as $plan )
+												<tr>
+													<td width="200">{{ $plan->title }}</td>
+													<td width="100">{{ $plan->percentage }}</td>
+													<td>{{ $plan->date }}</td>
+												</tr>
+											@endforeach
+										</table>
+									</td>
+									<td>
+										{{ $compare->options->project->expected_completion_date }}
+									</td>
+									<td>
+										<form method="POST" action="{{ route('compares.destroy', $compare->rowId) }}">
+											{{ csrf_field() }}
+											{!! method_field('DELETE') !!}
+											<div class="form-group">
+												<button type="submit" class="btn btn-danger">&times;</button>
+											</div>
+										</form>
+									</td>
+								</tr>
+							@endforeach
+						@else
 							<tr>
-								<td width="300">
-									<a href="{{ route('projects.show', $compare->options->project->slug) }}">
-										{{ sprintf('%s - %s', $compare->options->project->name, $compare->options->project->title) }}
-									</a> 
-									<small>
-										@foreach( $compare->options->project->types as $type )
-											<span class="label label-success">{{ $type->name }}</span>
-										@endforeach
-									</small>
-								</td>
-								<td>
-									<a href="{{ route('developers.show', $compare->options->project->developer->slug) }}">
-										{{ $compare->options->project->developer->name }}
-									</a>
-								</td>
-								<td>
-									@foreach( $compare->options->project->communities as $community )
-										{{ $community->name }}
-									@endforeach
-								</td>
-								<td>
-									<table class="table table-bordered table-striped">
-										@foreach( $compare->options->project->payments as $plan )
-											<tr>
-												<td width="200">{{ $plan->title }}</td>
-												<td width="100">{{ $plan->percentage }}</td>
-												<td>{{ $plan->date }}</td>
-											</tr>
-										@endforeach
-									</table>
-								</td>
-								<td>
-									{{ $compare->options->project->expected_completion_date }}
-								</td>
-								<td>
-									<form method="POST" action="{{ route('compares.destroy', $compare->rowId) }}">
-										{{ csrf_field() }}
-										{!! method_field('DELETE') !!}
-										<div class="form-group">
-											<button type="submit" class="btn btn-danger">&times;</button>
-										</div>
-									</form>
-								</td>
+								<td colspan="6">Please <a href="/projects">select a project to compare</a></td>
 							</tr>
-						@endforeach
+						@endif
 					</tbody>
 				</table>
 			</div>
