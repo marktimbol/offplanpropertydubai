6988edfa96b6f5b6bf22380b5561530a249dba19
Mark Timbol <Mark@Mark.local>
2016-10-30 17:13:53 +0400

    Remove registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Register</div>
-               
+                    <?php /*
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                             {{ csrf_field() }}
@@ -78,6 +78,7 @@
                             </div>
                         </form>
                     </div>
+                    */ ?>
                 </div>
             </div>
         </div>
