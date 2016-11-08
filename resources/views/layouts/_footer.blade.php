d2daa87bde8d6627f4d07d06f76b4be5724bd4c7
Mark Timbol <mark.timbol@hotmail.com>
2016-11-08 12:15:11 +0400

    Do not show developers on the project page

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,35 +1,38 @@
 
-@if( count($developers) > 0 )
-	<section class="DeveloperListings--container">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h3 class="Section__title text-center">Developers</h3>
-					<div class="DeveloperListings Carousel">
-						@foreach( $developers as $developer )
-						<?php 
-							$path = '/images/no-developer-image.jpg';
-							if( $developer->photo != '' ) {
-								$path = getPhotoPath($developer->photo);
-							}
-						?>
-						<div class="DeveloperListing">
-							<div class="DeveloperListing__image">
-								<a href="{{ route('developers.show', $developer->slug) }}">
-									<img src="{{ $path }}" 
-										alt="{{ $developer->name }}" 
-										title="{{ $developer->Name }}" 
-										class="img-responsive" />
-								</a>
+@if( Route::current()->getName() != 'developers.projects.show' )
+	@if( count($developers) > 0 )
+		<section class="DeveloperListings--container">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3 class="Section__title text-center">Developers</h3>
+						<div class="DeveloperListings Carousel">
+							@foreach( $developers as $developer )
+							<?php 
+								$path = '/images/no-developer-image.jpg';
+								if( $developer->photo != '' ) {
+									$path = getPhotoPath($developer->photo);
+								}
+							?>
+							<div class="DeveloperListing">
+								<div class="DeveloperListing__image">
+									<a href="{{ route('developers.show', $developer->slug) }}">
+										<img src="{{ $path }}" 
+											alt="{{ $developer->name }}" 
+											title="{{ $developer->Name }}" 
+											class="img-responsive" />
+									</a>
+								</div>
 							</div>
+							@endforeach
 						</div>
-						@endforeach
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
+	@endif
 @endif
 
-
 <footer></footer>
+
+
