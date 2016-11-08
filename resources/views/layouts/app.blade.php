9c17ed33afa85e3a5c2f24d0ebc2a9cc93b6fc45
Mark Timbol <mark.timbol@hotmail.com>
2016-11-08 11:59:19 +0400

    Add google-site-verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="google-site-verification" content="1XbV53OyHuAyPbbPAZsDF7kOfd-bBW2wy6gZ4XPFjUU" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
