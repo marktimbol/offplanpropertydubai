165fccbc6cd4307d52fec2a41a58e853930e01be
Mark <mark@Marks-MacBook-Pro.local>
2016-12-10 12:57:17 +0400

    Add community photo

--- resources/views/public/communities/index.blade.php
+++ resources/views/public/communities/index.blade.php
@@ -15,25 +15,26 @@
 				<div class="Communities">
 					@foreach( $communities as $community )
 						<div class="Community">
-							<div class="row">
-								<div class="Community__image col-md-6">
-									@if( $community->photo != '' )
-										<img src="{{ getPhotoPath($community->photo) }}" alt="" title="" class="img-responsive" />
-									@else
-									
-									@endif
-								</div>
-								<div class="Community__content col-md-6">
-									<h2>{{ $community->name }}</h2>
-									<p class="lead">
-										{{ str_limit($community->description, 250, '...') }}
-									</p>
+							<div class="Flex Flex--center">
+								@if( $community->photo != '' )
+									<div class="Column-6 Community__image">
+										<img src="{{ getPhotoPath($community->photo) }}" alt="" title="" class="img-responsive" />									
+									</div>
+									<div class="Column-6 Community__content--container">
+								@else
+									<div class="Column-12 Community__content--container Community--no-image">
+								@endif
+									<div class="Community__content">
+										<h2>{{ $community->name }}</h2>
+										<p class="lead">
+											{{ str_limit($community->description, 250, '...') }}
+										</p>
+									</div>
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-12">
-									<p>&nbsp;</p>
-									<h4>Available in {{ $community->name }}</h4>
+									<h4>Projects in {{ $community->name }}</h4>
 									@include('public.projects._listings', [
 										'projects'	=> $community->projects->take(3)
 									])
