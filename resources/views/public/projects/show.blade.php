9c23f2647ef76678266134820bc0b8b1c1b95f36
Mark Timbol <mark.timbol@hotmail.com>
2017-01-16 16:36:54 +0400

    Removed extra image from the project slideshow

--- resources/views/public/projects/show.blade.php
+++ resources/views/public/projects/show.blade.php
@@ -31,10 +31,7 @@
 								</div>
 								<?php /*
 								<div class="item lazyOwl" data-src="{{ getPhotoPath($photo->photo) }}"></div>
-								*/ ?>
-								<div>
-									<img src="/images/header-bg.jpg" alt="" title="" class="img-responsive" />
-								</div>									
+								*/ ?>								
 							@empty
 								<div>
 									<img src="/images/header-bg.jpg" alt="" title="" class="img-responsive" />
