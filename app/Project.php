3ffbf18301f22bcfb884709f78275e38627b0ae4
Mark Timbol <mark.timbol@hotmail.com>
2017-04-08 17:48:28 +0400

    Remove translatable trait for now.

--- app/Project.php
+++ app/Project.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    use Translatable;
+    // use Translatable;
     // use Searchable, Translatable;
 
     protected $fillable = [
