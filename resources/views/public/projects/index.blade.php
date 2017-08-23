f55b4b2c8f41dedee716a2f0981bd4bc330de040
markbelpro <mark@belprodigital.com>
2017-08-23 18:42:35 +0400

    Add search form

--- resources/views/public/projects/index.blade.php
+++ resources/views/public/projects/index.blade.php
@@ -9,10 +9,24 @@
 				<div class="col-md-12">
 					<h1 class="ProjectListings__title">
 						@lang('nav.projects')
-					</h1>
+					</h1>			
 					<p class="lead">
 						When it comes to finding out about off-plan, just launched or under-construction real estate projects in Dubai, our ‘new projects section’ is just what you need. Home to Dubai’s comprehensive list of real estate projects every project listing provides in-depth details starting from artistic, live images and video gallery, fact sheet, location map, frequently asked questions (FAQs), and more on sub communities within the projects, their features and nearby places. Our aim to provide as much information as possible to help you understand thoroughly as you progress with your home buying and investments. 
 					</p>
+				</div>
+			</div>
+
+			<div class="row Projects__search">
+				<div class="col-md-5">
+					<form method="GET" action="{{ route('search') }}" class="Search">
+						<label>@lang('messages.search-project')</label>
+						<div class="input-group">
+							<input type="text" name="query" class="form-control Search__input" />
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-primary">Search</button>
+							</span>
+						</div>
+					</form>
 				</div>
 			</div>
 
--- resources/views/public/search.blade.php
+++ resources/views/public/search.blade.php
@@ -8,8 +8,22 @@
 			<div class="row">
 				<div class="col-md-12">
 					<h1 class="ProjectListings__title">
-						{{ sprintf('Search results for %s', $query) }}
+						{!! sprintf('Search results for <em>%s</em>', $query) !!}
 					</h1>
+				</div>
+			</div>
+
+			<div class="row Projects__search">
+				<div class="col-md-5">
+					<form method="GET" action="{{ route('search') }}" class="Search">
+						<label>@lang('messages.search-project')</label>
+						<div class="input-group">
+							<input type="text" name="query" class="form-control Search__input" />
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-primary">Search</button>
+							</span>
+						</div>
+					</form>
 				</div>
 			</div>
 
